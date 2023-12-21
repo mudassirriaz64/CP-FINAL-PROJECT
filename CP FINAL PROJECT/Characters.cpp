313c1c5158993ca5e0736ed792d941734143ac9c
Mudassir Riaz <riazmudassir64@gmail.com>
2023-12-21 20:52:37 +0500

    Final Characters

--- CP FINAL PROJECT/Characters.cpp
+++ CP FINAL PROJECT/Characters.cpp
@@ -1,54 +1,77 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-struct character_information // Will store information about character
-{
-		string character_name;
-		string character_class;
-		int experience_level=0;
-	int character_health=0;
-}character;
-
-static void take_input_character(struct character_information)
-{
-	cout << "Enter character name : ";
-	cin >> character.character_name;
-	do {
-		cout << "Choose Class : Wage, Rogue Or Warrior" << endl;
-		cin >> character.character_class;
-		if (character.character_class == "wage")
-		{
-			character.character_health = 120;
-			character.experience_level = 3;
-		}
-		else if (character.character_class == "rogue")
-		{
-			character.character_health = 150;
-			character.experience_level = 5;
-		}
-		else if (character.character_class == "warrior")
-		{
-			character.character_health = 200;
-			character.experience_level = 9;
-		}
-		else
-		{
-			cout << "Invalid Class, please try again." << endl;
-		}
-	} while (character.character_class != "wage" && character.character_class != "rogue" && character.character_class != "warrior");
-}
-void display_character(struct character_information) //Display Character Information
-{
-	cout << "Character Name : " << character.character_name << endl;
-	cout << "Character Class : " << character.character_class << endl;
-	cout << "Character Health : " << character.character_health << endl;
-	cout << "Character Experience Level : " << character.experience_level << endl;
-}
-int main()
-{
-	cout << "\t \t \t Welcome To The Game" << endl;
-	take_input_character(character);
-	display_character(character);
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//struct character_information // will store information about character
+//{
+//		int hero_choose=0;
+//		string hero_name;
+//		string hero_class;
+//		int hero_experience_level=0;
+//		int hero_health=0;
+//		string hero_weapon;
+//		int hero_damage;
+//}character;
+//
+//static void take_input_character(struct character_information)
+//{
+//	cout << "You have to select hero from one of the following hero,Each of the character has its own weapone and style)" << endl;
+//	cout << "1. Valkriye ";
+//	cout<< "Fights with an axe around her,Tough melee Fighter and deals area damage around her " << endl;// Enter description of valkriye
+//	cout << "2. Magic Archer"<< endl;
+//	cout<<"Not quite a Wizard, nor an Archer - he shoots a magic arrow that passes through and damages all the enemies in its path"<<endl; //Enter description of gaint
+//	cout << "3. Prince " << endl;
+//	cout << "The prince rides an horse and deals area damage and lets his spiked club do the talking for him" << endl;// Enter desciption of Hog Rider
+//	do {
+//		cout << "Select your character(e,g 1,2,3) : ";
+//		cin >> character.hero_choose;
+//		if (character.hero_choose == 1)
+//		{
+//			character.hero_name = "Valkriye";
+//			character.hero_class = "Fighter";
+//			character.hero_health = 1584;
+//			character.hero_experience_level = 0;
+//			character.hero_weapon = "Axe";
+//			character.hero_damage = 221;
+//		}
+//		else if (character.hero_choose == 2)
+//		{
+//			character.hero_name = "Magic Archer";
+//			character.hero_class = "Archer";
+//			character.hero_health = 1326;
+//			character.hero_experience_level = 0;
+//			character.hero_weapon = "Bow And Magic Arrow";
+//			character.hero_damage = 134;
+//		}
+//		else if (character.hero_choose == 3)
+//		{
+//			character.hero_name = "Prince";
+//			character.hero_class = "Knight";
+//			character.hero_health = 1087;
+//			character.hero_experience_level = 0;
+//			character.hero_weapon = "Spiked Club";
+//			character.hero_damage = 170;
+//		}
+//		else
+//		{
+//			cout << "Invalid choice, please try again." << endl;
+//		}
+//	} while (character.hero_choose != 1 && character.hero_choose != 2 && character.hero_choose != 3);
+//}
+//void display_character(struct character_information) //display character information
+//{
+//	cout << "Hero Name : " << character.hero_name << endl;
+//	cout << "Class : " << character.hero_class << endl;
+//	cout << "Health : " << character.hero_health << endl;
+//	cout << "Experience Level : " << character.hero_experience_level << endl;
+//	cout << "Weapon : " << character.hero_weapon << endl;
+//	cout << "Damage : " << character.hero_damage << endl;
+//	
+//}
+//int main()
+//{
+//	cout << "\t \t \t welcome to the game" << endl;
+//	take_input_character(character);
+//	display_character(character);
+//	return 0;
+//}
---
+++ CP FINAL PROJECT/final boss.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//struct final_boss
+//{
+//	int health=5000;
+//	int damage;
+//	string weapon;
+//	int level = 10;
+//};/*"The final Boss" as the name suggsts this time the hero has come face to face with 
+//the final boss aka GODZILLA he is the king of all monsters and was tired of watching his people 
+//die by the hands of the hero. So he decided to personally kill the hero to end all the fuss***
+//The battle took place on the top of a mountain where no man can dare to reach. The hero is full of
+//spirit and his determined to face the monster(GOdzilla) and kill him.Godzilla who was also the king 
+//was also looking insane and held nothing back.The battle Starts in the gaazzling winds above the 
+//mountain and the fate of humanity and the monsters rests on the shoulders of these two BEASTS.*/
+//
+//
+//};
