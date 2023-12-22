d974ebd70e4d982780e615a322d4fa5e2dd83b5f
Mudassir Riaz <riazmudassir64@gmail.com>
2023-12-22 22:22:32 +0500

    Character and quest combined

--- CP FINAL PROJECT/Characters.cpp
+++ CP FINAL PROJECT/Characters.cpp
@@ -1,77 +1,283 @@
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//
-//struct character_information // will store information about character
-//{
-//		int hero_choose=0;
-//		string hero_name;
-//		string hero_class;
-//		int hero_experience_level=0;
-//		int hero_health=0;
-//		string hero_weapon;
-//		int hero_damage;
-//}character;
-//
-//static void take_input_character(struct character_information)
-//{
-//	cout << "You have to select hero from one of the following hero,Each of the character has its own weapone and style)" << endl;
-//	cout << "1. Valkriye ";
-//	cout<< "Fights with an axe around her,Tough melee Fighter and deals area damage around her " << endl;// Enter description of valkriye
-//	cout << "2. Magic Archer"<< endl;
-//	cout<<"Not quite a Wizard, nor an Archer - he shoots a magic arrow that passes through and damages all the enemies in its path"<<endl; //Enter description of gaint
-//	cout << "3. Prince " << endl;
-//	cout << "The prince rides an horse and deals area damage and lets his spiked club do the talking for him" << endl;// Enter desciption of Hog Rider
-//	do {
-//		cout << "Select your character(e,g 1,2,3) : ";
-//		cin >> character.hero_choose;
-//		if (character.hero_choose == 1)
-//		{
-//			character.hero_name = "Valkriye";
-//			character.hero_class = "Fighter";
-//			character.hero_health = 1584;
-//			character.hero_experience_level = 0;
-//			character.hero_weapon = "Axe";
-//			character.hero_damage = 221;
-//		}
-//		else if (character.hero_choose == 2)
-//		{
-//			character.hero_name = "Magic Archer";
-//			character.hero_class = "Archer";
-//			character.hero_health = 1326;
-//			character.hero_experience_level = 0;
-//			character.hero_weapon = "Bow And Magic Arrow";
-//			character.hero_damage = 134;
-//		}
-//		else if (character.hero_choose == 3)
-//		{
-//			character.hero_name = "Prince";
-//			character.hero_class = "Knight";
-//			character.hero_health = 1087;
-//			character.hero_experience_level = 0;
-//			character.hero_weapon = "Spiked Club";
-//			character.hero_damage = 170;
-//		}
-//		else
-//		{
-//			cout << "Invalid choice, please try again." << endl;
-//		}
-//	} while (character.hero_choose != 1 && character.hero_choose != 2 && character.hero_choose != 3);
-//}
-//void display_character(struct character_information) //display character information
-//{
-//	cout << "Hero Name : " << character.hero_name << endl;
-//	cout << "Class : " << character.hero_class << endl;
-//	cout << "Health : " << character.hero_health << endl;
-//	cout << "Experience Level : " << character.hero_experience_level << endl;
-//	cout << "Weapon : " << character.hero_weapon << endl;
-//	cout << "Damage : " << character.hero_damage << endl;
-//	
-//}
-//int main()
-//{
-//	cout << "\t \t \t welcome to the game" << endl;
-//	take_input_character(character);
-//	display_character(character);
-//	return 0;
-//}
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct character_information // STORES INFORMATION ABOUT THE HERO OR CHARACTER
+{
+	int hero_choose = 0;
+	string hero_name;
+	string hero_class;
+	int hero_experience_level = 0;
+	int hero_health = 0;
+	string hero_weapon;
+	int hero_damage;
+}character;
+
+struct quest_1_boss // STORES INFORMATION ABOUT QUEST 1 BOSS WHICH IS MONSTER
+{
+	int health;
+	int damage;
+	int level;
+}monster;
+
+struct quest_2_boss // STORES INFORMATION ABOUT QUEST 2 BOSS WHICH IS THE KING
+{
+	int health;
+	int damage;
+	int level;
+}king;
+
+struct quest_3_boss // STORES INFORMATION ABOUT QUEST 3 BOSS WHICH IS GUNSMITH
+{
+	int health;
+	int damage;
+	int level;
+}gunsmith;
+
+struct quest_4_boss // STORES INFORMATION ABOUT QUEST 4 BOSS WHICH IS A DRAGON
+{
+	int health;
+	int damage;
+	int level;
+}dragon;
+
+// FUNCTIONS PROTOYPES OF ALL THE FUNCTIONS USED IN THE PROGRAM
+void quest_selection_function();
+static void take_input_character(struct character_information);
+void display_character(struct character_information);
+void quest_1_function(char& quest_choice, struct quest_1_boss);
+void quest_2_function(char& quest_choice, struct quest_2_boss); 
+void quest_3_function(char& quest_choice, struct quest_3_boss); 
+void quest_4_function(char& quest_choice, struct quest_4_boss); 
+
+// INT MAIN
+int main()
+{
+	cout << "\t \t \t welcome to the game" << endl;
+	take_input_character(character);
+	quest_selection_function();
+	return 0;
+}
+
+// THIS FUNCTION SELECTS THE HERO AND OTHER INFORMATION RELATED TO HERO
+static void take_input_character(struct character_information)
+{
+	cout << "You have to select hero from one of the following hero,Each of the character has its own weapone and style)" << endl;
+	cout << "1. Valkriye " << endl;
+	cout << "Fights with an axe around her,Tough melee Fighter and deals area damage around her " << endl;// Enter description of valkriye
+	cout << "2. Magic Archer" << endl;
+	cout << "Not quite a Wizard, nor an Archer - he shoots a magic arrow that passes through and damages all the enemies in its path" << endl; //Enter description of gaint
+	cout << "3. Prince " << endl;
+	cout << "The prince rides an horse and deals area damage and lets his spiked club do the talking for him" << endl;// Enter desciption of Hog Rider
+	do {
+		cout << "Select your character(e,g 1,2,3) : ";
+		cin >> character.hero_choose;
+		if (character.hero_choose == 1)
+		{
+			character.hero_name = "Valkriye";
+			character.hero_class = "Fighter";
+			character.hero_health = 1584;
+			character.hero_experience_level = 0;
+			character.hero_weapon = "Axe";
+			character.hero_damage = 221;
+		}
+		else if (character.hero_choose == 2)
+		{
+			character.hero_name = "Magic Archer";
+			character.hero_class = "Archer";
+			character.hero_health = 1326;
+			character.hero_experience_level = 0;
+			character.hero_weapon = "Bow And Magic Arrow";
+			character.hero_damage = 134;
+		}
+		else if (character.hero_choose == 3)
+		{
+			character.hero_name = "Prince";
+			character.hero_class = "Knight";
+			character.hero_health = 1087;
+			character.hero_experience_level = 0;
+			character.hero_weapon = "Spiked Club";
+			character.hero_damage = 170;
+		}
+		else
+		{
+			cout << "Invalid choice, please try again." << endl;
+		}
+	} while (character.hero_choose != 1 && character.hero_choose != 2 && character.hero_choose != 3);
+}
+
+void display_character(struct character_information) // DISPLAY HERO OR CHARACTER INFORMATION
+{
+	cout << "Hero Name : " << character.hero_name << endl;
+	cout << "Class : " << character.hero_class << endl;
+	cout << "Health : " << character.hero_health << endl;
+	cout << "Experience Level : " << character.hero_experience_level << endl;
+	cout << "Weapon : " << character.hero_weapon << endl;
+	cout << "Damage : " << character.hero_damage << endl;
+}
+
+// THIS FUNCTION STARTS THE QUEST BASED ON USER CHOICE
+void quest_selection_function()
+{
+	char quest_choice;
+	cout << "\t \"You can Embark on different Quests.\"\n" << endl
+		<< "\t\nEach quest have three types of difficulty level\n";
+	cout << "\nYou can select the quest and then the difficulty level on which you want to play.\n";
+	do {
+		cout << "\n A.The forgotten Race.";
+		cout << "\t B.Army of the king.";
+		cout << "\t C.The jury.";
+		cout << "\t D.The king of dragons.";
+		cout << "\nEnter the quest you want to play (e.g. A,B): ";
+		cin >> quest_choice;
+		if (islower(quest_choice))
+		{
+			quest_choice = toupper(quest_choice);
+		}
+		switch (quest_choice)
+		{
+
+		case 'A':
+		{
+			cout << "\t The forgotten Race.";
+			quest_1_function(quest_choice, monster);
+			break;
+		}
+		case 'B':
+		{
+			cout << "\t Army of the king.";
+			quest_2_function(quest_choice, king);
+			break;
+		}
+		case 'C':
+		{
+			cout << "\t The jury.";
+			quest_3_function(quest_choice, gunsmith);
+			break;
+		}
+		case 'D':
+		{
+			cout << "\t The king of dragons.";
+			quest_4_function(quest_choice, dragon);
+			break;
+		}
+		default: cout << "Invalid Selection, please select again : ";
+		}
+	} while (quest_choice != 'A' && quest_choice != 'B' && quest_choice != 'C' && quest_choice != 'D');
+}
+
+// FUNCTION OF QUEST 1
+void quest_1_function(char& quest_choice, struct quest_1_boss)
+{
+	int difficulty;
+	string quest_1_message = " This Quest was the battle in which the forgotten race namely"
+		"the race of dragons which were thought to be extinct millions of years "
+		"ago but our hero was not shocked to see them as he had battles with monsters "
+		"and oher creatures but this time the hero was also frightened as the dragon was "
+		"enormous and had a menacing aura due to which the hero was not feeling well.Still "
+		"He fights for everyone and is determined to defeat the monster.Fight Starts";
+	cout << "\nSelect The difficulty on which you want to play it.\n(1).Easy\n(2).Medium\n(3).Hard\nchoose e.g(1,2):";
+	cin >> difficulty;
+	display_character(character);
+	if (difficulty == 1)
+	{
+		cout << "\nYour difficulty level is Easy.";
+		cout << "\nThe quest will Start soon. ";
+		cout << "\n\n Attackrand()";
+
+
+	}
+	else if (difficulty == 2) {
+		cout << "\nYour difficulty level is Medium.";
+	}
+	else if (difficulty == 3) {
+		cout << "\nYour difficulty level is Hard.";
+	}
+}
+
+// FUNCTION OF QUEST 2
+void quest_2_function(char& quest_choice, struct quest_2_boss)
+{
+	int difficulty;
+	string quest_1_message = " This Quest was the battle in which the forgotten race namely"
+		"the race of dragons which were thought to be extinct millions of years "
+		"ago but our hero was not shocked to see them as he had battles with monsters "
+		"and oher creatures but this time the hero was also frightened as the dragon was "
+		"enormous and had a menacing aura due to which the hero was not feeling well.Still "
+		"He fights for everyone and is determined to defeat the monster.Fight Starts";
+	cout << "\nSelect The difficulty on which you want to play it.\n(1).Easy\n(2).Medium\n(3).Hard\nchoose e.g(1,2):";
+	cin >> difficulty;
+	display_character(character);
+	if (difficulty == 1)
+	{
+		cout << "\nYour difficulty level is Easy.";
+		cout << "\nThe quest will Start soon. ";
+		cout << "\n\n Attackrand()";
+
+
+	}
+	else if (difficulty == 2) {
+		cout << "\nYour difficulty level is Medium.";
+	}
+	else if (difficulty == 3) {
+		cout << "\nYour difficulty level is Hard.";
+	}
+}
+
+// FUNCTION OF QUEST 3
+void quest_3_function(char& quest_choice, struct quest_3_boss)
+{
+	int difficulty;
+	string quest_1_message = " This Quest was the battle in which the forgotten race namely"
+		"the race of dragons which were thought to be extinct millions of years "
+		"ago but our hero was not shocked to see them as he had battles with monsters "
+		"and oher creatures but this time the hero was also frightened as the dragon was "
+		"enormous and had a menacing aura due to which the hero was not feeling well.Still "
+		"He fights for everyone and is determined to defeat the monster.Fight Starts";
+	cout << "\nSelect The difficulty on which you want to play it.\n(1).Easy\n(2).Medium\n(3).Hard\nchoose e.g(1,2):";
+	cin >> difficulty;
+	display_character(character);
+	if (difficulty == 1)
+	{
+		cout << "\nYour difficulty level is Easy.";
+		cout << "\nThe quest will Start soon. ";
+		cout << "\n\n Attackrand()";
+
+
+	}
+	else if (difficulty == 2) {
+		cout << "\nYour difficulty level is Medium.";
+	}
+	else if (difficulty == 3) {
+		cout << "\nYour difficulty level is Hard.";
+	}
+}
+
+// FUNCTION OF QUEST 4
+void quest_4_function(char& quest_choice, struct quest_4_boss)
+{
+	int difficulty;
+	string quest_1_message = " This Quest was the battle in which the forgotten race namely"
+		"the race of dragons which were thought to be extinct millions of years "
+		"ago but our hero was not shocked to see them as he had battles with monsters "
+		"and oher creatures but this time the hero was also frightened as the dragon was "
+		"enormous and had a menacing aura due to which the hero was not feeling well.Still "
+		"He fights for everyone and is determined to defeat the monster.Fight Starts";
+	cout << "\nSelect The difficulty on which you want to play it.\n(1).Easy\n(2).Medium\n(3).Hard\nchoose e.g(1,2):";
+	cin >> difficulty;
+	display_character(character);
+	if (difficulty == 1)
+	{
+		cout << "\nYour difficulty level is Easy.";
+		cout << "\nThe quest will Start soon. ";
+		cout << "\n\n Attackrand()";
+
+
+	}
+	else if (difficulty == 2) {
+		cout << "\nYour difficulty level is Medium.";
+	}
+	else if (difficulty == 3) {
+		cout << "\nYour difficulty level is Hard.";
+	}
+}
--- CP FINAL PROJECT/final boss.cpp
+++ CP FINAL PROJECT/final boss.cpp
@@ -3,17 +3,13 @@
 //using namespace std;
 //struct final_boss
 //{
-//	int health=5000;
-//	int damage;
-//	string weapon;
+//	int health=3000;
+//	int damage=450;
+//	string weapon= "Meelee";
 //	int level = 10;
-//};/*"The final Boss" as the name suggsts this time the hero has come face to face with 
-//the final boss aka GODZILLA he is the king of all monsters and was tired of watching his people 
-//die by the hands of the hero. So he decided to personally kill the hero to end all the fuss***
-//The battle took place on the top of a mountain where no man can dare to reach. The hero is full of
-//spirit and his determined to face the monster(GOdzilla) and kill him.Godzilla who was also the king 
-//was also looking insane and held nothing back.The battle Starts in the gaazzling winds above the 
-//mountain and the fate of humanity and the monsters rests on the shoulders of these two BEASTS.*/
-//
-//
+//};
+//void final_boss_function(struct final_boss)
+//{
+//	string message = "Well done on getting such a long way, now a final boss awaits in the arrival of the hero, the final boss and the hero has come face to face ,the final boss aka GODZILLA, he is the king of all monsters and was tired of watching his peopledie by the hands of an ordinary hero.So he decided to personally kill the hero to end all the fuss. The battle took place on the top of a mountain where no man could dare to reach.The hero is full of spirit and his determined to face the monster(Godzilla) and kill him.Godzilla the king was also looking insane and held nothing back. The battle starts in the dazzling winds above the mountain and the fate of humanity and the monsters rests on the shoulders of these two beasts.";
+//	
 //};
